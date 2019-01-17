e7a4f30d38429ef3e9b27582840f1bd9d75ff74e
eriro <eriro@axis.com>
2019-01-17 15:31:47 +0100

    Fix more

--- GotoMark.sh
+++ GotoMark.sh
@@ -1,3 +1,2 @@
-pos=$(grep $1 ~/.marks | sed -s s/$1://)
-echo "Going to mark $1 which is: $pos"
-echo $pos
+path=$(grep $1 ~/.marks | sed -s s/$1://)
+echo $path
--- PlaceMark.sh
+++ PlaceMark.sh
@@ -1,5 +1,5 @@
 echo "Placing mark '$1' at $PWD"
 regex="$1:.*"
 echo "REGEX $regex"
-sed -i "/$1:.*/d" ~/.marks > ~/.marks
+sed -i "/$regex/d" ~/.marks > ~/.marks
 echo "$1:$PWD" >> ~/.marks
