ad6b18af730994a7ceff8bee29b4693843fdedb3
eriro <eriro@axis.com>
2019-01-18 12:23:49 +0100

    Fix regex

--- PlaceMark.sh
+++ PlaceMark.sh
@@ -1,4 +1,4 @@
 echo "Placing mark '$1' at $PWD"
 regex="$1:.*"
-sed -i "/$regex/d" ~/.marks
+sed -i "/^$regex/d" ~/.marks
 echo "$1:$PWD" >> ~/.marks
--- PlaceMarkMacOS.sh
+++ PlaceMarkMacOS.sh
@@ -1,4 +1,4 @@
 echo "Placing mark '$1' at $PWD"
 regex="$1:.*"
-gsed -i "/$regex/d" ~/.marks
+gsed -i "/^$regex/d" ~/.marks
 echo "$1:$PWD" >> ~/.marks
